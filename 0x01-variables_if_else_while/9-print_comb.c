f031b381115611e61eee48a627a32dfac0ce23d7
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-11 16:51:37 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - prints Numbers
+* main - Entry point
 * Return: Always 0 (Success)
 */
 
@@ -13,11 +13,10 @@ int a;
 for (a = '0'; a <= '9'; a++)
 {
 putchar(a);
-}
 if (a != '9')
-{
 putchar(',');
 putchar('');
+}
 }
 putchar('\n');
 return (0);
