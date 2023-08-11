437c6427c8dd5b3bf38f336f9be3a4dd5d4f369e
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-11 11:11:46 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("The last digit of %d is %d and is less than 6 and not 0\n", n, x);
 }
 else
 {
-printf(The last digit of %d is %d and is 0\n", n, x);
+printf("The last digit of %d is %d and is 0\n", n, x);
 }
 return (0);
 }
