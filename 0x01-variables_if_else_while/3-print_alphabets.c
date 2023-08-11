083afb2551093400d987718e847d76054d45b115
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-11 14:01:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main prints the alphABET
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 'a';
+int j = 'A';
+while ( i <= 'z')
+{
+putchar (i);
+i++;
+}
+while ( j <= 'Z')
+{
+putchar (j);
+j++;
+}
+putchar('\n');
+return (0);
+}
