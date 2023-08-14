3dcf22e3e46efaf342c0a00fbc7964585f017707
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-14 11:27:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,22 @@
 
 int main(void)
 {
-int d, p, q;
-for (d = 0; d < 9; d++)
+int a, p, q;
+for (a = '0'; a < '9'; a++)
 {
-for (p = d+1; p < 9; p++)
+for (p = a + 1; p <= '9'; p++)
 {
-for (q = p+1; q < 9; q++)
+for (q = p + 1; q <= '9'; q++)
 {
-if ((p != d) != q)
+if ((p != a) != q)
 {
-putchar(d);
+putchar(a);
 putchar(p);
 putchar(q);
-if (d == 7 && p == 8)
-{
+if (a == '7' && p == '8')
 continue;
-}
 putchar(',');
-putchar('');
+putchar(' ');
 }
 }
 }
