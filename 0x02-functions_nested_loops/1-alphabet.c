073edaa33daa6fde10da0092be97d7cd970273eb
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-15 08:18:20 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-#include <main.h>
+#include "main.h"
 
 /**
-* print_alphabet - Make the alphabet
-* Return: void
+* print _alphabet - print all alphabet in lowercase
 */
 
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
 _putchar('\n');
 }
