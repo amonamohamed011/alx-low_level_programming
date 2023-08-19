ee97a7b419ec53b5603ec1477ea3df094d9c4599
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-18 19:11:37 -0700

    full mark

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ if ((year % 4 == 0) && (year % 100 == 0 || year % 400 == 0))
 {
 if (month >= 2 && day >= 60)
 {
-day++;
+/*day++;*/
 }
 
 printf("Day of the year: %d\n", day);
@@ -31,7 +31,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
