b8488afe32dba012e3e905624e6c299168c83dd8
amonamohamed011 <amonamohamed011@gmail.com>
2023-08-25 13:08:51 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,17 @@
 #include <stdio.h>
 
 /**
-* main - prints Numbers
+* main - prints numbers
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int a;
-for (a = '0'; a <= '9'; a++)
+for (a = '0'; a < 10; a++)
 {
 putchar(a);
 }
-putchar('\n');
+putchar("\n");
 return (0);
 }
