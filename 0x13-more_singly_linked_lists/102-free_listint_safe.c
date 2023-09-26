1afec050683ae57eb5ace89a5a893725ba76d4ba
amonamohamed011 <amonamohamed011@gmail.com>
2023-09-26 11:45:28 -0700

    a

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -22,7 +22,7 @@ return (end->next);
 return (NULL);
 }
 /**
-* size_t free_listint_safe - frees a listint_t list
+* free_listint_safe - frees a listint_t list
 * @h: pointer to the first node in the linked list
 *
 * Return: number of elements in the freed list
