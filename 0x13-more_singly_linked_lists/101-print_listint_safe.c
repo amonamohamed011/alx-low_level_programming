35fccad477d1cced53af96d7bef1e89c30a4a154
amonamohamed011 <amonamohamed011@gmail.com>
2023-09-26 08:32:17 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -51,7 +51,7 @@ return (0);
 */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes = 0;
+size_t nodes, index = 0;
 nodes = looped_listint_len(head);
 if (nodes == 0)
 {
@@ -60,7 +60,15 @@ for (; head != NULL; nodes++)
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
-printf("->[%p] %d\n", (void *)head, head->n);
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
 }
 return (nodes);
 }
