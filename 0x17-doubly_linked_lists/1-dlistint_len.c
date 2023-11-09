526dcfd26d44c87897f7328914481f375b539efb
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-09 11:38:24 -0800

    b

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,13 @@
 #include "lists.h"
+
 /**
-* size_t distint_len - function that returns the number of elements in a,
-* linked distint_t list
-* @h: pointer to the head of the list
+* dlistint_len - function that returns the number of elements in a,
+* linked dlistint_t list.
+* @h: poiner to the head of the list
 *
 * Return: number of elements
 */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 size_t count = 0;
