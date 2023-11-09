11b2dfbdca8a81c472ab545939e3f8018dddd86a
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-09 10:03:09 -0800

    e

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_dlistint - function that frees a dbly
+* linked list
+* @head: pointer to head of list
+*
+* Return: no return
+*/
+void free_dlistint(dlistint_t *head)
+{
+while (head != NULL)
+{
+dlistint_t *next = head->next;
+free(head);
+head = next;
+}
+}
